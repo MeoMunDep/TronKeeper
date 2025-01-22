8214992413ecf46546d9a28dbdcf839948e0a736
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-22 13:01:32 +0700

    ended

--- TronKeeper/setup.sh
+++ TronKeeper/setup.sh
@@ -115,7 +115,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            cd tronkeeper && node bot
+            node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
